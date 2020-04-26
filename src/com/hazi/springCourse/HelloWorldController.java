fe0966c287c8523c9bba7a22245d98f84fff3fa9
hdudekula <hazi.dudekula@gmail.com>
2020-04-26 13:26:55 +0530

    Revert "Revert "Revert "Request Params and Mapping Demo"""
    This reverts commit de502dded2a88d599bab584371fa47f5b83d661f.

--- src/com/hazi/springCourse/HelloWorldController.java
+++ src/com/hazi/springCourse/HelloWorldController.java
@@ -5,12 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-//We add Parent RequestMapping Annotation here to avoid the ambiguity with
-//any other controller classes. 
 @Controller
-@RequestMapping("/hello")
 public class HelloWorldController {
 
 	//Need a controller method to show the initial HTML form 
@@ -31,23 +27,18 @@ public class HelloWorldController {
 	//Need a controller method to read form data and
 	//add data to model
 	
-	//RequestParam annotation is used to bind the data
-	@RequestMapping("/processFormVersionThree")
-	public String processVersionThree(
-			@RequestParam("StudentName") String theName, 
-			Model model) {
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
 		
-	/*below lines 38 - 41 has been replaced by RequestParam annotation in line 34 */
-		/*
-		 * //Read the request parameter from a HTML form String theName =
-		 * request.getParameter("StudentName");
-		 */
+	
+	//Read the request parameter from a HTML form
+		String theName = request.getParameter("StudentName");
 	
 	//convert the read data to caps
 		theName = theName.toUpperCase();
 	
 	//create a message
-		String result = "I am coming from V3! " + theName;
+		String result = "Yo! " + theName;
 	
 	
 	//add data to model
